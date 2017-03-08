c6a27bdb15c2d27ad76bc29a9af91d1874514f6a
pomazawi <mulaczos@gmail.com>
2017-03-08 08:19:39 +0100

    angular works, homepage from LoginController

--- src/main/java/shop/infrastructure/security/controller/UserController.java
+++ src/main/java/shop/infrastructure/security/controller/UserController.java
@@ -2,6 +2,7 @@ package shop.infrastructure.security.controller;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.security.Principal;
@@ -10,7 +11,8 @@ import java.security.Principal;
  * Created by Witu on 07.03.2017.
  */
 @RequiredArgsConstructor
-@RestController("authentication")
+@RestController
+@RequestMapping("authentication")
 public class UserController {
 
     @GetMapping
--- src/main/resources/static/js/LoginController.js
+++ src/main/resources/static/js/LoginController.js
@@ -1,6 +1,10 @@
+var app = angular.module("LoginController", [])
+
 app.controller('LoginController', [ '$scope', function($scope) {
 
         $scope.init = function() {
             console.log("it works!");
         }
+
+        $scope.greeting = "hello!";
 }]);
