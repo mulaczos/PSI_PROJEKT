8461c6806b8776d0d141a0b24e29fd28c9d3ac7a
pomazawi <mulaczos@gmail.com>
2017-03-08 07:31:02 +0100

    deleted cloned classes

--- src/main/java/shop/infrastructure/repository/CustomerRepository.java
+++
@@ -1,7 +0,0 @@
-package shop.infrastructure.repository;
-
-import org.springframework.data.repository.CrudRepository;
-import shop.infrastructure.model.Customer;
-
-public interface CustomerRepository extends CrudRepository<Customer, Long> {
-}
