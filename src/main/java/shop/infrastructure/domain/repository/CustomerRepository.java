0cee979c87a2ad2fede254b18e79f94c2e22e754
pomazawi <witold.pomazanka@ista.pl>
2017-04-06 15:21:06 +0200

    added crud methods for Customer

--- src/main/java/shop/infrastructure/domain/repository/CustomerRepository.java
+++ src/main/java/shop/infrastructure/domain/repository/CustomerRepository.java
@@ -4,6 +4,9 @@ import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 import shop.infrastructure.domain.model.Customer;
 
+import java.util.List;
+
 @Repository
 public interface CustomerRepository extends CrudRepository<Customer, Long> {
+	List<Customer> findAll();
 }
--- src/main/java/shop/infrastructure/domain/service/CustomerService.java
+++ src/main/java/shop/infrastructure/domain/service/CustomerService.java
@@ -5,16 +5,31 @@ import org.springframework.stereotype.Service;
 import shop.infrastructure.domain.model.Customer;
 import shop.infrastructure.domain.repository.CustomerRepository;
 
-/**
- * Created by Witu on 04.03.2017.
- */
+import java.util.List;
+
 @Service
 public class CustomerService {
-
-    @Autowired
-    private CustomerRepository customerRepository;
-
-    public Customer save(Customer customer) {
-        return customerRepository.save(customer);
-    }
+	
+	@Autowired
+	private CustomerRepository customerRepository;
+	
+	public Customer get(Long id) {
+		return customerRepository.findOne(id);
+	}
+	
+	public List<Customer> getAll() {
+		return customerRepository.findAll();
+	}
+	
+	public Customer save(Customer customer) {
+		return customerRepository.save(customer);
+	}
+	
+	public Customer update(Customer customer) {
+		return customerRepository.save(customer);
+	}
+	
+	public void delete(Long id) {
+		customerRepository.delete(id);
+	}
 }
