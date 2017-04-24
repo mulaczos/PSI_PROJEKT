f31106064fd9f46b7ce70c0b5d612eacb14cd080
mulaczos <mulaczos@gmail.com>
2017-04-24 20:18:43 +0200

    profile form works well

--- src/main/resources/public/js/core/own/ProfileController.js
+++ src/main/resources/public/js/core/own/ProfileController.js
@@ -14,15 +14,20 @@
             $scope.lastname = success.data.lastname;
             $scope.name = success.data.name;
             $scope.email = success.data.email;
-
+            $scope.password = null;
+            $scope.confirm = null;
         });
 
         $scope.arePasswordsSame = function () {
             if ($scope.password && $scope.confirm && $scope.password.length > 5 && $scope.confirm.length > 5) {
                 return $scope.password == $scope.confirm;
             } else {
-                return false;
+                return true;
             }
+        };
+
+        $scope.unlockSubmit = function(valid) {
+            return $scope.password && $scope.confirm && $scope.password.length > 5 && $scope.confirm.length > 5 && ($scope.password === $scope.confirm) && valid;
         };
     }
 }());
