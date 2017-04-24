3ca5b452fe138c5138e1b767920ff84869253be6
mulaczos <mulaczos@gmail.com>
2017-04-24 19:32:30 +0200

    profile form incrased but register now its not working..

--- src/main/resources/public/js/core/own/ProfileController.js
+++ src/main/resources/public/js/core/own/ProfileController.js
@@ -9,49 +9,21 @@
 
     function ProfileController($scope, AccountService, $rootScope) {
 
-        $scope.username = 'success.username';
-        $scope.lastname = 'success.lastname';
-        $scope.name = 'success.name';
-        $scope.email = 'success.email';
-        console.log("AAAAAAA");
-
         AccountService.getProfile().then(function (success) {
-            console.log(success.data);
-            console.log(success.data);
-            console.log(success.data);
-            console.log(success.data);
-            console.log(success.data);
-            console.log(success.data);
-            console.log(success.data);
-            console.log("IS THIS THING ON??");
             $scope.username = success.data.username;
             $scope.lastname = success.data.lastname;
             $scope.name = success.data.name;
             $scope.email = success.data.email;
-            console.log($scope.name);
-            console.log($scope.name);
-            console.log($scope.name);
-            console.log($scope.name);
-            console.log($scope.name);
-            console.log($scope.name);
-            console.log($scope.name);
-            console.log($scope.name);
 
         });
-        console.log("BBB");
 
-        // $rootScope.$on('$includeContentLoaded', function () {
-        //     $scope.init();
-        // });
-        // $scope.init = function () {
-        //     AccountService.getProfile().then(function (success) {
-        //         $scope.username = success.username;
-        //         $scope.lastname = success.lastname;
-        //         $scope.name = success.name;
-        //         $scope.email = success.email;
-        //     });
-        // };
-
+        $scope.arePasswordsTheSame = function () {
+            if ($scope.password && $scope.confirm && $scope.password.length > 5 && $scope.confirm.length > 5) {
+                return $scope.password == $scope.confirm;
+            } else {
+                return false;
+            }
+        };
     }
 }());
 
